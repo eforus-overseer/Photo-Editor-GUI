bdbde170a0acf67ec2355fafbf977b1652611f4a
Ef1Pe <40206908+eforus-overseer@users.noreply.github.com>
2026-09-16 11:09:25 +0300

    Update image_processing.py

--- image_processing.py
+++ image_processing.py
@@ -1,5 +1,4 @@
-#   Name: Efi Pecani
-#   ID: 307765230 
+############## IMAGE PROCESSING ##############
 
 from tkinter import *         #import all of this stuff in order to wor kwith tkinter methods
 import tkinter as tk
